c10ba415c156caa0c88e1bd17160979d03f8f009
Thomas Silloway <tcsillow@gmail.com>
2024-11-07 20:57:05 -0500

    Add support for rebuilding only zone files to speed up iteration

--- rebuild_zone.bat
+++ rebuild_zone.bat
@@ -2,4 +2,5 @@
 title ZONE
 REM shutdown world and zone with docker-compose then rebuild zone after that's complete
 
-wsl bash -c "cd /mnt/c/GithubRepos/QuarmDocker && docker-compose stop zone world && docker-compose up --build zone"
+start wsl bash -c "cd /mnt/c/GithubRepos/QuarmDocker && docker-compose stop world"
+wsl bash -c "cd /mnt/c/GithubRepos/QuarmDocker && docker-compose stop zone && docker-compose up --build zone"
